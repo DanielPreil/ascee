13ac148affc25dab877810889032673c77bb68be
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2023-08-11 20:00:50 +0200

    feat: Index.js: asceeCreateCanvas: takes care of setting the canvas element on the renderer, background Transparency, scene, Canvas Pixelated Render Optimization, camera Setting + Z axis width tha Parameters: CanvasElement: String, TransparentBackground: Boolean, Camera Aspect Ratio: Number, Camera Near: Number, Camera Far: Number, Camera Z Axis: Number -> it returns the renderer, scene, camera variables to the user that can be passed to the next function called asceeCreateModel which displays the modelElement on the screen, updates the scene (+ Basic Animations). And at the end there is the asceeCreateLight function that helps creating DirectionalLights by passing these Parameters: asceeScene: THREE.Scene, lightStrength: Number, lightColor: Hexadecimal Number, lightPosition.x: Number, lightPosition.y: Number, lightPosition.z: Number. The Code 95% is Documented Out With Helper Comments clarifying what how to to.

--- package/index.js
+++ package/index.js
@@ -2,43 +2,46 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-const asceeCreate = (canvasElement, modelElement) => {
-  
+// Create The asceeCanvas With The parameters which User Passes or use The Defaults
+// Parameters: CanvasElement: String, TransparentBackground: Boolean, Camera Aspect Ratio: Number, Camera Near: Number, Camera Far: Number, Camera Z Axis: Number
+const asceeCreateCanvas = (canvasElement, transparent=false, cameraFov=75, cameraAspectRatio, cameraNear=0.1, cameraFar=1000, cameraZ=2.5) => {
+  // Setup renderer -> CanvasElement + Transparent or not Background
+  const renderer = new THREE.WebGLRenderer({ canvas: canvasElement, alpha: transparent });
+  // Create The Scene
   const scene = new THREE.Scene();
-  const renderer = new THREE.WebGLRenderer({ canvas: canvasElement, alpha: true });
+  // --- Canvas Pixelated Render Optimization ---
+  canvasElement.width = window.innerWidth;
+  canvasElement.height = window.innerHeight;
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(canvasElement.clientWidth, canvasElement.clientHeight);
+  // --- Canvas Pixelated Render Optimization Over ---
+  // Create the camera variable based on the provided parameters of the asceeCreateCanvas function
+  const camera = new THREE.PerspectiveCamera(cameraFov, cameraAspectRatio, cameraNear, cameraFar);
+  // Camera Z Axis
+  camera.position.z = cameraZ;
+  
+  // Return With renderer, scene, camera values
+  return { renderer, scene, camera }
+}
+
+// Create The asceeModel With The parameters which User Passes or use The Defaults
+// Parameters: modelElement: String, renderer: THREE.WebGLRenderer scene: THREE.Scene, camera: THREE.PerspectiveCamera
+const asceeCreateModel = (modelElement, renderer, scene, camera) => {
+
+  // Create The Model variable + add it to the scene
   const model = new THREE.Group();
   scene.add(model);
-
-  // Load the GLB model
+  
+  // Load User's (Gltf / Glb) Model + add it to the model variable (That is already passed to the scene)
   const loader = new GLTFLoader();
   loader.load(modelElement, (gltf) => {
     const loadedModel = gltf.scene;
     model.add(loadedModel);
   });
-
-  // Increase canvas size
-  canvasElement.width = window.innerWidth;
-  canvasElement.height = window.innerHeight;
-
-  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 3);
-  directionalLight1.position.set(0, 1, 0);
-  scene.add(directionalLight1);
-
-  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 3);
-  directionalLight2.position.set(0, 0, 1);
-  scene.add(directionalLight2);
-
-  const directionalLight3 = new THREE.DirectionalLight(0xffffff, 3);
-  directionalLight3.position.set(1, 0, 0);
-  scene.add(directionalLight3);
   
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(canvasElement.clientWidth, canvasElement.clientHeight);
-  
-  const camera = new THREE.PerspectiveCamera( 75, canvasElement.clientWidth / canvasElement.clientHeight, 0.1, 1000);
-  
-  camera.position.z = 2.5;
-  
+
+ // --- Uncommented Parts
+
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
   controls.update();
@@ -56,6 +59,20 @@ const asceeCreate = (canvasElement, modelElement) => {
   }
 
   animate();
+
+ // --- Uncommented Parts Over
 }
 
-export default asceeCreate;
+// Create Light With The parameters which User Passes or use The Defaults
+// Parameters: asceeScene: THREE.Scene, lightStrength: Number, lightColor: Hexadecimal Number, lightPosition.x: Number, lightPosition.y: Number, lightPosition.z: Number
+const asceeCreateLight = (scene, lightStrength=3, lightColor=0xffffff, lightPosition={ x: 0, y: 1, z: 0}) => {
+  // Create a Light Soruce From The parameters by using the Color and The Strength
+  const light = new THREE.DirectionalLight(lightColor, lightStrength);
+  // Then Set it's Position (x, y, z)
+  light.position.set(lightPosition.x, lightPosition.y, lightPosition.z);
+  // At the end add it to the scene that User provided
+  scene.add(light);
+}
+
+// Export asceeCreateCanvas, asceeCreateModel, asceeCreateLight Functions
+export { asceeCreateCanvas, asceeCreateModel, asceeCreateLight };
--- tests/script.js
+++ tests/script.js
@@ -1,8 +1,24 @@
-import asceeCreate from "./node_modules/ascee/index.js"
+import { asceeCreateCanvas, asceeCreateModel, asceeCreateLight } from "./node_modules/ascee/index.js"
 
 window.addEventListener("DOMContentLoaded", () => {
-  const canvasElement = document.querySelector(".myCanvas")
-
+  
+  // Grab The Model, Canvas
   const modelElement = "./room.glb"
-  asceeCreate(canvasElement, modelElement)
+  const canvasElement = document.querySelector(".myCanvas")
+  // Create The asceeCanvas With The parameters or use The Defaults
+  // Parameters: CanvasElement: String, TransparentBackground: Boolean, Camera Aspect Ratio: Number, Camera Near: Number, Camera Far: Number,  Camera Z Axis: Number
+  const asceeCanvas = asceeCreateCanvas(canvasElement, true, 75, canvasElement.clientWidth / canvasElement.clientHeight, 0.1, 1000, 2.5)
+  // asceeCanvas Object Destructuring (Get The: renderer, scene, camera)
+  const asceeRenderer = asceeCanvas.renderer
+  const asceeScene = asceeCanvas.scene
+  const asceeCamera = asceeCanvas.camera
+  // pass Those Values Into the asceeCreateModel Function That Takes care the things you have provided to
+  // create the model of yours and display it on the scene
+  asceeCreateModel(modelElement, asceeRenderer, asceeScene, asceeCamera)
+  // asceeCreateLight Functions Take Care of creating Light Points, Users Can have Multiple ones in one scene
+  // Parameters: asceeScene: THREE.Scene, lightStrength: Number, lightColor: Hexadecimal Number, lightPosition.x: Number, lightPosition.y: Number, lightPosition.z: Number
+  asceeCreateLight(asceeScene, 3, 0xffffff, { x: 0, y: 0, z: 1 })
+  asceeCreateLight(asceeScene, 3, 0xffffff, { x: 0, y: 1, z: 0 })
+  asceeCreateLight(asceeScene, 3, 0xffffff, { x: 1, y: 0, z: 0 })
+
 })
